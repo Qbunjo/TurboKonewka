9be807e02a6816759f9fce303b10b4019bf4c942
Qbunjo <39863704+Qbunjo@users.noreply.github.com>
2020-02-28 15:36:32 +0100

    Update wahrsager.ino

--- wahrsager.ino
+++ wahrsager.ino
@@ -67,6 +67,13 @@ digitalWrite(redLedPin,LOW);
 digitalWrite(greenLedPin,HIGH);
 }
 void grabjson(){
+  curl "http://api.openweathermap.org/data/2.5/forecast?q=Stare Babice,PL&mode=json&APPID=5b2c2ccce162451f435efd1a2ba7ed1f&units=metric"
 }
 void parsejson(){
+  //forecast is for 3 hours, so we have to check 3x8=24h
+  for (int i=0, i==7, i++){
+  forecast=list[i];
+    //check for rain
+    //if rain, rainfall=rainfall+rainvalue;
+  }
 }
