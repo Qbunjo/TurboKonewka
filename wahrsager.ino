b19b275d45eed53df4f31d16a9da83a633da6b5b
Qbunjo <39863704+Qbunjo@users.noreply.github.com>
2020-02-28 11:13:29 +0100

    Update wahrsager.ino

--- wahrsager.ino
+++ wahrsager.ino
@@ -9,15 +9,28 @@
 // for relay
 //define variables
 
+int t=0,tH=0 tM=0; //time variables
+
+const char *ssid     = "SSID";
+const char *password = "PASS";
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", hour, 60000);
+
 void setup (){
-//wifi setup
-//first grab of timestamp trom ntp
+Serial.begin(115200);
+WiFi.begin(ssid, password); 
+
+  while ( WiFi.status() != WL_CONNECTED ) {
+    delay ( 500 );
+    Serial.print ( "." );
+  }
+  timeClient.begin();
 
 }
 void loop(){
 // grab timestamp
 //check 
-if (timestamp = 005){ //five minutes after midnight it grabs owm info
+if (t > 000 and t <006){ //five minutes after midnight it grabs owm info
 grabjson
 parsejson =>output rainfall
 if (rainfall >= 06) {
@@ -35,6 +48,10 @@ ClockLed HIGH;
 }//loop end
 
 void timestamp(){
+   timeClient.update();
+  tH=timeClient.getHours();
+  tM=timeClient.getMinutes();
+  t = (tH* 100) + tM; 
 }
 void stopwatering(){
 //switch off the relay
