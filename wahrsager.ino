5fdb388ab87ac3f3dfbd00816238ec545d72641d
Qbunjo <39863704+Qbunjo@users.noreply.github.com>
2020-02-28 08:36:35 +0100

    Create wahrsager.ino
    Totally init version with pseudocode

---
+++ wahrsager.ino
@@ -0,0 +1,46 @@
+//TODO
+#include NTP;
+#include NTPUDP;
+#include json;
+#include WIFI8266;
+
+//define pins
+// for led 
+// for relay
+//define variables
+
+void setup (){
+//wifi setup
+//first grab of timestamp trom ntp
+
+}
+void loop(){
+// grab timestamp
+//check 
+if (timestamp = 005){
+grabjson
+parsejson =>output rainfall
+if (rainfall >= 06) {
+stopwatering}
+else {startwatering}
+
+}
+for (TimeWait=0, TimeWait==3000, TimeWait+2){
+delay(1000);//wait for 1 second
+ClockLed LOW;
+delay(1000);//wait for 1 second
+ClockLed HIGH;
+}
+
+}//loop end
+
+void timestamp(){
+}
+void stopwatering(){
+//switch off the relay
+//turn on red led, turn off green led}
+void startwatering(){//switch on the relay, turn off red led, turn on green led}
+void grabjson(){
+}
+void {parsejson(){
+}
