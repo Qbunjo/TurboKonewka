d9476cecda1459818d9f40ddaba3d2b2b92cb81d
Qbunjo <39863704+Qbunjo@users.noreply.github.com>
2020-02-29 15:48:05 +0100

    Update wahrsager.ino

--- wahrsager.ino
+++ wahrsager.ino
@@ -1,20 +1,21 @@
-#include NTP.h;
-#include NTPUDP.h;
-#include ArduinoJson.h;
-#include WIFI8266.h;
+#include <NTPClient.h>;
+#include <Wifiudp.h>;
+#include <ArduinoJson.h>;
+#include <ESP8266WiFi.h>;
 
 //define pins
 // for led 
 // for relay
 //define variables
+int RedLedPin=4,GreenLedPin=5,RelayPin=6;
 
-int t=0,tH=0 tM=0; //time variables
+int t=0,tH=0,tM=0; //time variables
 double rainfall;
 
 const char *ssid     = "SSID";
 const char *password = "PASS";
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", hour, 60000);
+NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 3600, 60000);
 
 void setup (){
 Serial.begin(115200);
@@ -32,18 +33,15 @@ t=timestamp();
 // grab timestamp
 //check 
 if (t > 000 and t <006){ //five minutes after midnight it grabs owm info
-grabjson
-parsejson =>output rainfall
+grabjson();
+parsejson() =>output rainfall
 if (rainfall >= 06) {
 stopwatering}
 else {startwatering}
 
 }
-for (TimeWait=0, TimeWait==3000, TimeWait+2){ //five minutes of wait
+for (int TimeWait=0; TimeWait==2999; TimeWait++){ //five minutes of wait
 delay(1000);//wait for 1 second
-ClockLed LOW;
-delay(1000);//wait for 1 second
-ClockLed HIGH;
 }
 
 }//loop end
@@ -56,22 +54,22 @@ int timestamp(){
 }
 void stopwatering(){
 digitalWrite(RelayPin,HIGH);
-digitalWrite(redLedPin,HIGH);
-digitalWrite(greenLedPin,LOW);
+digitalWrite(RedLedPin,HIGH);
+digitalWrite(GreenLedPin,LOW);
   //switch off the relay
 //turn on red led, turn off green led
 }
 void startwatering(){
-digitalWrite(RelayPin,LOw);
-digitalWrite(redLedPin,LOW);
-digitalWrite(greenLedPin,HIGH);
+digitalWrite(RelayPin,LOW);
+digitalWrite(RedLedPin,LOW);
+digitalWrite(GreenLedPin,HIGH);
 }
 void grabjson(){
-  curl "http://api.openweathermap.org/data/2.5/forecast?q=Stare Babice,PL&mode=json&APPID=5b2c2ccce162451f435efd1a2ba7ed1f&units=metric"
+  curl "http://api.openweathermap.org/data/2.5/forecast?q=Stare Babice,PL&mode=json&APPID=5b2c2ccce162451f435efd1a2ba7ed1f&units=metric" ;
 }
 void parsejson(){
   //forecast is for 3 hours, so we have to check 3x8=24h
-  for (int i=0, i==7, i++){
+  for (int i=0; i==7; i++){
   forecast=list[i];
     //check for rain
     //if rain, rainfall=rainfall+rainvalue;
