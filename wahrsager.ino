28c726af39c8300130d6a5cd98b5f7dde2866add
Qbunjo <39863704+Qbunjo@users.noreply.github.com>
2020-03-02 14:24:34 +0100

    Update wahrsager.ino

--- wahrsager.ino
+++ wahrsager.ino
@@ -12,6 +12,16 @@ int RedLedPin=4,GreenLedPin=5,RelayPin=6;
 int t=0,tH=0,tM=0; //time variables
 double rainfall;
 
+#define max_readings 8
+
+Forecast_record_type  WxConditions[1];
+Forecast_record_type  WxForecast[max_readings];
+
+#include "common.h"
+
+float rain_readings[max_readings]        = {0};
+
+
 const char *ssid     = "SSID";
 const char *password = "PASS";
 WiFiUDP ntpUDP;
@@ -34,7 +44,7 @@ t=timestamp();
 //check 
 if (t > 000 and t <006){ //five minutes after midnight it grabs owm info
 grabjson();
-parsejson() =>output rainfall
+rainfall=parsejson();
 if (rainfall >= 06) {
 stopwatering}
 else {startwatering}
@@ -65,13 +75,18 @@ digitalWrite(RedLedPin,LOW);
 digitalWrite(GreenLedPin,HIGH);
 }
 void grabjson(){
-  curl "http://api.openweathermap.org/data/2.5/forecast?q=CITY,CT&mode=json&APPID=APIAPI&units=metric" ;
+   byte Attempts = 1;
+  while ((RxForecast == false) && Attempts <= 2) { // Try up-to twice for Forecast data
+              if (RxForecast == false) RxForecast = obtain_wx_data(client, "forecast");
+        Attempts++;
+      }
 }
-void parsejson(){
-  //forecast is for 3 hours, so we have to check 3x8=24h
-  for (int i=0; i==7; i++){
-  forecast=list[i];
-    //check for rain
-    //if rain, rainfall=rainfall+rainvalue;
+float parseJson(){
+  do {   
+     rain_readings[r]     = WxForecast[r].Rainfall;
+     float rainFall=rainFall+rain_readings[r];
+    r++;
+  } while (r <= max_readings);
+return rainFall;}
   }
 }
